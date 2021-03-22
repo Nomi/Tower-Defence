1a463aca12a4fe32fdec70dc363d28532eb9bb1f
Nomi <baba.noman@live.com>
2021-03-22 16:37:11 +0100

    Implemented the RatCatcher class and the Attack function.

--- RatCatcher.cs
+++ RatCatcher.cs
@@ -13,5 +13,44 @@ namespace Defenders
             this.name = name;
             hasRat=false;
         }
+
+        int IDefender.Attack(Rat e)
+        {
+            Console.WriteLine(e.GetType() + " " + e.Name + " approaches " + this.GetType() + " " + this.name + ".");
+            if (hasRat)
+            {
+                Console.WriteLine(this.GetType() + " " + this.name + " ignores " + e.GetType() + " " + e.Name);
+                return 0;
+            }
+            else
+            {
+                Console.WriteLine(this.GetType() + " " + this.name + " catches " + e.GetType() + " " + e.Name);
+                hasRat = true;
+                return 99999999;        //assuming that max health is less than 99999999 then the enemy will die for sure.
+            }
+        }
+
+        int IDefender.Attack(Giant e)
+        {
+            Console.WriteLine(e.GetType() + " " + e.Name + " approaches " + this.GetType() + " " + this.name + ".");
+            Console.WriteLine(this.GetType() + " " + this.name + " ignores " + e.GetType() + " " + e.Name+'.');
+            return 0;
+        }
+
+        int IDefender.Attack(Ogre e)
+        {
+            Console.WriteLine(e.GetType() + " " + e.Name + " approaches " + this.GetType() + " " + this.name + ".");
+            if (!hasRat)
+            {
+                Console.WriteLine(this.GetType() + " " + this.name + " ignores " + e.GetType() + " " + e.Name + '.');
+                return 0;
+            }
+            else
+            {
+                Console.WriteLine(this.GetType() + " " + this.name + " throws the rat it has on " + e.GetType() + " " + e.Name + " causing it to flee.");
+                hasRat = false;
+                return 99999999;        //assuming that max health is less than 99999999 then the enemy will die for sure.
+            }
+        }
     }
 }
